87cb90c51bcc867fb1e30a3140c616c9ba932406
e5021620 <e5021620@TCLHSRNVALT0273>
2026-05-22 11:40:03 +0530

    Testing commit and push

--- Git_basics/Form1.cs
+++ Git_basics/Form1.cs
@@ -20,6 +20,7 @@ namespace Git_basics
         private void Form1_Load(object sender, EventArgs e)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Testing commit and push");
         }
     }
 }
